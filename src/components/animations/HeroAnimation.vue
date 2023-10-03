bc19e18a03d53d2fcc7d35603bf05e2fe98da6b6
pawel.g <pawel.g@rsstudio.net>
2023-10-03 20:27:53 +0200

    hero animation component

---
+++ src/components/animations/HeroAnimation.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="hero-animation">
+        <div class="hero-animation__item" v-for="item in 40"></div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.hero-animation {
+    width: 100%;
+    height: 100%;
+    position: sticky;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    background-color: rgb(24, 24, 24);
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    transform: rotateZ(45deg);
+
+    &__item {
+        width: 100%;
+        height: 100%;
+        border: 1px solid #FFB800;
+        border-radius: 8px;
+    }
+}
+</style>
