3908c54742c9b5c1aec6707e0e8db29cf1df8b39
galkowskip <galkowski.p97@gmail.com>
2023-10-28 18:33:12 +0200

    projects list pagination added

--- src/types.ts
+++ src/types.ts
@@ -6,4 +6,38 @@ interface ListItem {
     id: string;
 }
 
-export type { ListItem };
+interface GithubProject {
+    id: string;
+    name: string;
+    description: string;
+    html_url: string;
+    homepage: string;
+    language: string;
+    stargazers_count: number;
+    forks_count: number;
+    watchers_count: number;
+    open_issues_count: number;
+    created_at: string;
+    updated_at: string;
+    pushed_at: string;
+    license: {
+        key: string;
+        name: string;
+        spdx_id: string;
+        url: string;
+        node_id: string;
+    };
+    owner: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        type: string;
+        site_admin: boolean;
+    };
+}
+
+export type { ListItem, GithubProject };
