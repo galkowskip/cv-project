ade6f8647ccf38c8f1db4d488dece1a38eadd8d1
galap <galkowski.p97@gmail.com>
2023-10-29 19:15:01 +0100

    contact box shadows fix

--- src/components/ContactBox.vue
+++ src/components/ContactBox.vue
@@ -51,7 +51,7 @@ import LinkedinIcon from "./icons/LinkedinIcon.vue";
     width: 100%;
     height: 100%;
     border-radius: 8px;
-    box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.2);
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
     background-color: #fff;
     padding: 24px;
     display: flex;
